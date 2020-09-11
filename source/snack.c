212298f27639fc000d5c86f7c2def49f637bb4b9
Your Name <you@example.com>
2020-09-11 16:53:45 +0800

    添加ncurses库到工程

--- source/snack.c
+++ source/snack.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <curses.h>
+
+void init_game()
+{
+    initscr();
+}
+
+void game_loop()
+{
+}
+
+void game_score()
+{
+    printf("game over\n");
+    endwin();
+}
 
 int main()
 {
-    printf("hell snack\n");
+    /* 初始化游戏 */
+    init_game();
+
+    /* 游戏主循环 */
+    game_loop();
+
+    /* 游戏得分 */
+    game_score();
+
     return 0;
 }
